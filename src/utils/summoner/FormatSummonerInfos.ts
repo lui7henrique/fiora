db590eac5a19b42355eaad4c3b25ff0cf1f90c08
Luiz Henrique <7henrique18@gmail.com>
2022-01-02 15:54:48 -0300

    :sparkles: Summoner: FormatSummonerInfos

---
+++ src/utils/summoner/FormatSummonerInfos.ts
@@ -0,0 +1,33 @@
+// full response https://developer.riotgames.com/apis#match-v5/GET_getMatch
+
+import { Match } from "types/match"
+
+export function FormatSummonerInfos(
+  summonerInfos: Match["info"]["participants"][0]
+) {
+  const formattedSummonerInfos = {
+    summoner: {
+      id: summonerInfos.summonerId,
+      name: summonerInfos.summonerName,
+      icon: `http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${summonerInfos.profileIcon}.png`
+    },
+    kda: `${summonerInfos.kills}/${summonerInfos.deaths}/${summonerInfos.assists}`,
+    champion: {
+      id: summonerInfos.championId,
+      name: summonerInfos.championName,
+      image: `https://raw.communitydragon.org/pbe/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${summonerInfos.championId}/${summonerInfos.championId}000.jpg`,
+      level: summonerInfos.champLevel
+    },
+    build: [
+      summonerInfos.item0,
+      summonerInfos.item1,
+      summonerInfos.item2,
+      summonerInfos.item3,
+      summonerInfos.item4,
+      summonerInfos.item5,
+      summonerInfos.item6
+    ]
+  }
+
+  return formattedSummonerInfos
+}
