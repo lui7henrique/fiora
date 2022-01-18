d716c4c769b986c893a4ecb4066670822955fce9
Luiz Henrique <7henrique18@gmail.com>
2022-01-18 14:04:23 -0300

    :gear: Change hotjar initialize

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,9 @@ import Document, {
 } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
+const hotjar =
+  "function(h,o,t,j,a,r){    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};    h._hjSettings={hjid:2788975,hjsv:6};    a=o.getElementsByTagName('head')[0];    r=o.createElement('script');r.async=1;    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;    a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -33,6 +36,7 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+
   render() {
     return (
       <Html lang="pt-BR">
@@ -42,6 +46,10 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
+          />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: hotjar }}
           />
         </Head>
         <body>
