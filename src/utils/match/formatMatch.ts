84bf46fc3c3f9cf112abafa8bd30008a53870d89
Luiz Henrique <7henrique18@gmail.com>
2022-02-28 21:44:46 -0300

    :gear: Enable team field on participants

--- src/utils/match/formatMatch.ts
+++ src/utils/match/formatMatch.ts
@@ -17,6 +17,56 @@ export const formatMatch = (matchInfo: Match["info"]) => {
     queueId
   } = matchInfo
 
+  const formattedParticipants = matchInfo.participants.map((participant) => {
+    return {
+      ...FormatSummonerInfos(participant),
+      // fight
+      kills: participant.kills,
+      deaths: participant.deaths,
+      assists: participant.assists,
+      largestKillingSpree: participant.largestKillingSpree,
+      largestMultiKill: participant.largestMultiKill,
+
+      // dmg
+      totalDamageDealtToChampions: participant.totalDamageDealtToChampions,
+      physicalDamageDealtToChampions:
+        participant.physicalDamageDealtToChampions,
+      magicDamageDealtToChampions: participant.magicDamageDealtToChampions,
+      trueDamageDealtToChampions: participant.trueDamageDealtToChampions,
+      totalDamageDealt: participant.totalDamageDealt,
+      physicalDamageDealt: participant.physicalDamageDealt,
+      magicDamageDealt: participant.magicDamageDealt,
+      trueDamageDealt: participant.trueDamageDealt,
+      largestCriticalStrike: participant.largestCriticalStrike,
+      damageDealtToObjectives: participant.damageDealtToObjectives,
+      damageDealtToTurrets: participant.damageDealtToTurrets,
+
+      // heal
+      totalHeal: participant.totalHeal,
+      totalDamageTaken: participant.totalDamageTaken,
+      physicalDamageTaken: participant.physicalDamageTaken,
+      magicDamageTaken: participant.magicDamageTaken,
+      trueDamageTaken: participant.trueDamageTaken,
+      damageSelfMitigated: participant.damageSelfMitigated,
+
+      // vision
+      visionScore: participant.visionScore,
+      wardsPlaced: participant.wardsPlaced,
+      wardsKilled: participant.wardsKilled,
+      detectorWardsPlaced: participant.detectorWardsPlaced,
+
+      // gold
+      goldEarned: participant.goldEarned,
+      goldSpent: participant.goldSpent,
+      totalMinionsKilled: participant.totalMinionsKilled,
+
+      // other
+      turretTakedowns: participant.turretTakedowns,
+      inhibitorTakedowns: participant.inhibitorTakedowns,
+      team: participant.teamId
+    }
+  })
+
   const formattedMatch = {
     gameCreation,
     gameDuration,
@@ -30,54 +80,7 @@ export const formatMatch = (matchInfo: Match["info"]) => {
     mapId,
     platformId,
     queueId,
-    participants: matchInfo.participants.map((participant) => {
-      return {
-        ...FormatSummonerInfos(participant),
-        // fight
-        kills: participant.kills,
-        deaths: participant.deaths,
-        assists: participant.assists,
-        largestKillingSpree: participant.largestKillingSpree,
-        largestMultiKill: participant.largestMultiKill,
-
-        // dmg
-        totalDamageDealtToChampions: participant.totalDamageDealtToChampions,
-        physicalDamageDealtToChampions:
-          participant.physicalDamageDealtToChampions,
-        magicDamageDealtToChampions: participant.magicDamageDealtToChampions,
-        trueDamageDealtToChampions: participant.trueDamageDealtToChampions,
-        totalDamageDealt: participant.totalDamageDealt,
-        physicalDamageDealt: participant.physicalDamageDealt,
-        magicDamageDealt: participant.magicDamageDealt,
-        trueDamageDealt: participant.trueDamageDealt,
-        largestCriticalStrike: participant.largestCriticalStrike,
-        damageDealtToObjectives: participant.damageDealtToObjectives,
-        damageDealtToTurrets: participant.damageDealtToTurrets,
-
-        // heal
-        totalHeal: participant.totalHeal,
-        totalDamageTaken: participant.totalDamageTaken,
-        physicalDamageTaken: participant.physicalDamageTaken,
-        magicDamageTaken: participant.magicDamageTaken,
-        trueDamageTaken: participant.trueDamageTaken,
-        damageSelfMitigated: participant.damageSelfMitigated,
-
-        // vision
-        visionScore: participant.visionScore,
-        wardsPlaced: participant.wardsPlaced,
-        wardsKilled: participant.wardsKilled,
-        detectorWardsPlaced: participant.detectorWardsPlaced,
-
-        // gold
-        goldEarned: participant.goldEarned,
-        goldSpent: participant.goldSpent,
-        totalMinionsKilled: participant.totalMinionsKilled,
-
-        // other
-        turretTakedowns: participant.turretTakedowns,
-        inhibitorTakedowns: participant.inhibitorTakedowns
-      }
-    })
+    participants: formattedParticipants
   }
 
   return formattedMatch
